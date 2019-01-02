9a1d6d3f10b7f7678d585312aaa0ba4e07bb426e
marelix2 <marelix2@gmail.com>
2019-01-02 12:50:29 +0100

    sold data completed

--- backend/config/routes/publicRoutes.js
+++ backend/config/routes/publicRoutes.js
@@ -1,5 +1,8 @@
 const publicRoutes = {
     'GET /login/google': 'AuthController.urlGoogle', 
     'POST /login/user': 'AuthController.authUser',
+
+    'PUT /sold/user': 'SoldDataController.getAllForDisplay',
+    'PUT /bought/user': 'BoughtDataController.getAllForDisplay',
 };
 module.exports = publicRoutes;
--- frontend/src/endpoints.js
+++ frontend/src/endpoints.js
@@ -3,8 +3,12 @@ const BASE_PRIVATE = "/private";
 
 const GET_GOOGLE_URL = BASE_PUBLIC + '/login/google';
 const POST_GOOGLE_CODE = BASE_PUBLIC + '/login/user';
+const PUT_ALL_SOLD_DATA = BASE_PUBLIC + '/sold/user';
+const PUT_ALL_BOUGHT_DATA = BASE_PUBLIC + '/bought/user';
 
 export default {
     GET_GOOGLE_URL,
     POST_GOOGLE_CODE,
+    PUT_ALL_SOLD_DATA,
+    PUT_ALL_BOUGHT_DATA,
 };
